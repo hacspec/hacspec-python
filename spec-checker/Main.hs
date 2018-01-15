ab68a50a308c2bd844d71f7226883dda6a5d7516
Aaron Tomb <atomb@galois.com>
2018-01-15 07:29:09 -0800

    Some location-based error messages

--- spec-checker/Main.hs
+++ spec-checker/Main.hs
@@ -1,63 +1,80 @@
 module Main where
 
 import Language.Python.Common.AST
+import Language.Python.Common.Pretty
+import Language.Python.Common.PrettyAST
+import Language.Python.Common.SrcLocation
 import Language.Python.Version3.Parser
 import System.Environment
 import System.Exit
 import System.IO
 
-validExpr :: Expr a -> Bool
-validExpr _ = False
-    
-validRaiseExpr :: RaiseExpr a -> Bool
-validRaiseExpr _ = False
+type CheckResult = Either String ()
 
-validStatement :: Statement a -> Bool
+good :: CheckResult
+good = Right ()
+
+bad :: (Pretty (t SrcSpan), Annotated t) => t SrcSpan -> CheckResult
+bad a =
+  Left $ "unsupported " ++ show (pretty a) ++ " at " ++ show (pretty (annot a))
+
+validOp :: Op SrcSpan -> CheckResult
+validOp op@(In _) = bad op
+validOp op@(Is _) = bad op
+validOp op@(IsNot _) = bad op
+validOp op@(NotIn _) = bad op
+validOp _ = good
+
+validExpr :: Expr SrcSpan -> CheckResult
+validExpr e = bad e
+
+variableExpr :: Expr SrcSpan -> CheckResult
+variableExpr (Var _ _) = good
+variableExpr e = bad e
+
+validArgument :: Argument SrcSpan -> CheckResult
+validArgument (ArgExpr e _) = validExpr e
+validArgument a = bad a
+    
+validStatementDecorator :: Decorator SrcSpan -> CheckResult
+validStatementDecorator d = bad d
+
+validStatement :: Statement SrcSpan -> CheckResult
 -- We assume a specific set of modules is already imported
 {-
 validStatement (While _ _ _ _) = validWhile
 validStatement (For _ _ _ _ _) = validFor
 validStatement (Fun _ _ _ _ _) = validFun
-validStatement (Conditional _ _ _) = validCond
 -}
-validStatement (Assign _ _ _) = True -- validAssign
-validStatement (AugmentedAssign _ _ _ _) = True --validAssign
-validStatement (Assert es _) = False --all validAssert es
-{-
-validStatement (Decorated _ _ _) = False
--}
-validStatement (Return _ _) = True -- validExpr e
-validStatement (Raise e _) = validRaiseExpr e
-validStatement (Pass _) = True
--- Statements we have no plans to support
-validStatement (Import _ _) = False -- We use a fixed set of imports
-validStatement (FromImport _ _ _) = False
-validStatement (Class _ _ _ _) = False
-validStatement (Try _ _ _ _ _) = False
-validStatement (With _ _ _) = False
-validStatement (Break _) = False
-validStatement (Continue _) = False
-validStatement (Delete _ _) = False
-validStatement (Global _ _) = False
-validStatement (NonLocal _ _) = False
-validStatement (Print _ _ _ _) = False
-validStatement (Exec _ _ _) = False
-validStatement _ = False
+validStatement (Conditional guards elseBody _) = do
+  mapM_ (validExpr . fst) guards
+  mapM_ (validSuite . snd) guards
+  validSuite elseBody
+validStatement (Assign es e _) = do
+    mapM_ variableExpr es >> validExpr e
+validStatement (AugmentedAssign l _ r _) =
+  variableExpr l >> validExpr r
+validStatement s@(Assert es _) = bad s --all validAssert es
+validStatement (Decorated ds s _) = mapM_ validStatementDecorator ds
+validStatement (Return me _) = maybe good validExpr me
+--validStatement (Raise e _) = validRaiseExpr e
+validStatement (Pass _) = good
+validStatement s = bad s
 
-validSuite :: Suite a -> Bool
-validSuite = all validStatement
+validSuite :: Suite SrcSpan -> CheckResult
+validSuite = mapM_ validStatement
 
-validParameter :: Parameter a -> Bool
-validParameter (Param _ _ Nothing _) = True
-validParameter _ = False
+validParameter :: Parameter SrcSpan -> CheckResult
+validParameter (Param _ _ Nothing _) = good
+validParameter p = bad p
 
-validTopStatement :: Statement a -> Bool
+validTopStatement :: Statement SrcSpan -> CheckResult
 validTopStatement (Fun _ ps _ body _) =
-  all validParameter ps && validSuite body
-validTopStatement _ = False
+  mapM_ validParameter ps >> validSuite body
+validTopStatement s = bad s
 
-validModule :: Module a -> Bool
-validModule (Module ss) = all validTopStatement ss
+validModule :: Module SrcSpan -> CheckResult
+validModule (Module ss) = mapM_ validTopStatement ss
 
 exitFail :: String -> IO ()
 exitFail msg = hPutStrLn stderr msg >> exitWith (ExitFailure 1)
@@ -69,8 +86,10 @@ checkFile filename = do
     Left err -> exitFail $ "Parse error: " ++ show err
     Right (m, _) -> do
       putStrLn "Parse succeeded!"
-      let status = if validModule m then "valid" else "NOT valid"
-      putStrLn $ "Module is " ++ status
+      let status = case validModule m of
+                     Right () -> "valid"
+                     Left msg -> "NOT valid: " ++ msg
+      putStrLn $ "File is " ++ status
 
 main :: IO ()
 main = do
