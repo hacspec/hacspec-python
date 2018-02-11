8b4dbf44c68e014cbecf6e4ab5df2824b75bd09c
Franziskus Kiefer <franziskuskiefer@gmail.com>
2018-02-11 19:01:32 +0100

    update check_schema

--- spec-checker/check_schema.py
+++ spec-checker/check_schema.py
@@ -5,21 +5,6 @@ import json
 # pip install jsonschema
 from jsonschema import validate
 
-poly1305_test_vectors = [
-{'input_len': '34',
- 'input'    : '43727970746f6772617068696320466f72756d2052657365617263682047726f7570',
- 'key'    : '85d6be7857556d337f4452fe42d506a80103808afb0db2fd4abff6af4149f51b',
- 'tag'    : 'a8061dc1305136c6c22b8baf0c0127a9'},
-{'input_len': '2',
- 'input'    : 'f3f6',
- 'key'    : '851fc40c3467ac0be05cc20404f3f700580b3b0f9447bb1e69d095b5928b6dbc',
- 'tag'    : 'f4c633c3044fc145f84f335cb81953de'},
-{'input_len': '0',
- 'input'    : '',
- 'key'    : 'a0f3080000f46400d0c7e9076c834403dd3fab2251f11ac759f0887129cc2ee7',
- 'tag'    : 'dd3fab2251f11ac759f0887129cc2ee7'}
- ]
-
 schema = {
   "type": "array",
   "items": {
@@ -54,8 +39,8 @@ if len(sys.argv) != 3:
 
 if sys.argv[1] == "mac":
     with open(sys.argv[2]) as json_data:
-        d = json.load(json_data)
-        validate(d, schema)
+        data = json.load(json_data)
+        validate(data, schema)
         print("Validation successful")
         sys.exit(0)
 
