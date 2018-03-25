b0f916c3e562b17b3880b922206368e9dfc1656d
Franziskus Kiefer <franziskuskiefer@gmail.com>
2018-03-25 18:27:16 +0200

    fix it and more tests

--- specs/wots.py
+++ specs/wots.py
@@ -29,6 +29,7 @@ address_t = array_t(uint32_t, 8)
 key_pair_t = Tuple[sk_t, pk_t, address_t]
 digest_t = bytes_t(32)
 seed_t = bytes_t(n)
+chain_t = Tuple[address_t, vlbytes_t]
 
 # F: SHA2-256(toByte(0, 32) || KEY || M),
 # H: SHA2-256(toByte(1, 32) || KEY || M),
@@ -96,7 +97,7 @@ def set_key_and_mask(adr: address_t, kam: uint32_t) -> address_t:
 # Output: value of F iterated s times on X
 
 
-def wots_chain(x: bytes_t, start: int, steps: int, seed: seed_t, adr: address_t) -> vlbytes_t:
+def wots_chain(x: bytes_t, start: int, steps: int, seed: seed_t, adr: address_t) -> chain_t:
     hmo = bytes.copy(x)
     for i in range(start, start + steps):
         adr = set_hash_address(adr, uint32(i))
@@ -108,7 +109,7 @@ def wots_chain(x: bytes_t, start: int, steps: int, seed: seed_t, adr: address_t)
         for (a, b) in zip(hmo, bm):
             fin = bytes.concat(fin, bytes([a ^ b]))
         hmo = F(key, fin)
-    return hmo
+    return adr, hmo
 
 
 def key_gen(adr: address_t, seed: seed_t) -> key_pair_t:
@@ -116,13 +117,11 @@ def key_gen(adr: address_t, seed: seed_t) -> key_pair_t:
     sk: sk_t = vlarray.create_type([], bytes)
     pk: pk_t = vlarray.create_type([], bytes)
     for i in range(0, uint32.to_int(length)):
-        # print(".", end="", flush=True)
         sk_i: bytes_t = bytes.create_random_bytes(n)
         adr = set_chain_address(adr, uint32(i))
-        pk_i: bytes_t = wots_chain(sk_i, 0, uint32.to_int(w)-1, seed, adr)
+        adr, pk_i = wots_chain(sk_i, 0, uint32.to_int(w)-1, seed, adr)
         sk = vlarray.concat(sk, sk_i)
         pk = vlarray.concat(pk, pk_i)
-    # print("")
     return (sk, pk, adr)
 
 
@@ -143,30 +142,7 @@ def base_w(msg: vlbytes_t, l: uint32_t) -> vlbytes_t:
         out = out + 1
     return basew
 
-
-def wots_sign(msg: digest_t, sk: sk_t, adr: address_t, seed: seed_t) -> sig_t:
-    csum = 0
-    m = base_w(msg, length1)
-    for i in range(0, uint32.to_int(length1)):
-        csum = csum + uint32.to_int(w) - 1 - uint32.to_int(m[i])
-    csum = csum << int(8 - ((uint32.to_int(length2) * log_w) % 8))
-    length2_bytes = ceil((uint32.to_int(length2) * log_w) / 8)
-    csum_bytes = bytes.from_nat_be(csum, length2_bytes)
-    tmp = base_w(csum_bytes, length2)
-    m = vlarray.concat(m, tmp)
-    sig: vlarray_t(bytes_t(n)) = vlarray.create_type([], bytes)
-    for i in range(0, uint32.to_int(length)):
-        # print(".", end="", flush=True)
-        adr = set_chain_address(adr, uint32(i))
-        # FIXME: why is this bytes needed here and not before?
-        sig_i: vlbytes_t = bytes(wots_chain(
-            sk[i], 0, uint32.to_int(m[i]), seed, adr))
-        sig = vlarray.concat(sig, sig_i)
-    # print("")
-    return sig
-
-
-def wots_verify(pk: pk_t, msg: digest_t, sig: sig_t, adr: address_t, seed: seed_t) -> Tuple[pk_t, address_t]:
+def wots_msg(msg: digest_t) -> vlbytes_t:
     csum = 0
     m = base_w(msg, length1)
     for i in range(0, uint32.to_int(length1)):
@@ -175,45 +151,25 @@ def wots_verify(pk: pk_t, msg: digest_t, sig: sig_t, adr: address_t, seed: seed_
     length2_bytes = ceil((uint32.to_int(length2) * log_w) / 8)
     csum_bytes = bytes.from_nat_be(csum, length2_bytes)
     m = array.concat(m, base_w(csum_bytes, length2))
+    return m
+
+
+def wots_sign(msg: digest_t, sk: sk_t, adr: address_t, seed: seed_t) -> sig_t:
+    m = wots_msg(msg, )
+    sig = vlarray.create_type([], bytes)
+    for i in range(0, uint32.to_int(length)):
+        adr = set_chain_address(adr, uint32(i))
+        adr, sig_i = wots_chain(sk[i], 0, uint32.to_int(m[i]), seed, adr)
+        sig = vlarray.concat(sig, sig_i)
+    return sig
+
+
+def wots_verify(pk: pk_t, msg: digest_t, sig: sig_t, adr: address_t, seed: seed_t) -> Tuple[pk_t, address_t]:
+    m = wots_msg(msg)
     pk2: pk_t = vlarray.create_type([], bytes)
     for i in range(0, uint32.to_int(length)):
         adr = set_chain_address(adr, uint32(i))
         m_i = uint32.to_int(m[i])
-        pk_i: vlbytes_t = wots_chain(
-            sig[i], m_i, uint32.to_int(w) - 1 - m_i, seed, adr)
+        adr, pk_i = wots_chain(sig[i], m_i, uint32.to_int(w) - 1 - m_i, seed, adr)
         pk2 = vlarray.concat(pk2, pk_i)
     return (pk2, adr)
-
-
-# def main(x: int) -> None:
-#     adr = array.create_random(8, uint32_t)
-#     seed = bytes.create_random_bytes(n)
-
-#     sk, pk, adr = key_gen(adr, seed)
-#     print("WOTS+ sk:\n" + str(sk))
-#     print("WOTS+ pk:\n" + str(pk))
-#     print("WOTS+ adr:\n" + str(adr))
-
-#     msg = bytes.from_ints([0xFF, 0xFF, 0xFF, 0xFF, 0xFF])
-#     msg_h = sha256(msg)
-#     sig = wots_sign(msg_h, sk, adr, seed)
-#     # print("WOTS+ sig")
-#     # print(sig)
-#     pk2 = wots_verify(pk, msg_h, sig, adr, seed)
-
-#     # Check pk
-#     verified = True
-#     if len(pk) != len(pk2):
-#         verified = False
-#     if verified:
-#         for (k1, k2) in zip(pk, pk2):
-#             if k1 != k2:
-#                 verified = False
-#                 break
-#     if verified:
-#         print("Signature verified")
-#     else:
-#         print("Verification error :(")
-
-# if __name__ == "__main__":
-#     main(0)
