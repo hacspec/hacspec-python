6366936de145839d35127b833088435d408d3a06
Denis Merigoux <denis.merigoux@gmail.com>
2018-03-29 16:22:52 +0200

    Working spec for blake2b

---
+++ specs/test_vectors/blake2b_test_vectors.py
@@ -0,0 +1,16 @@
+from mypy_extensions import TypedDict
+from speclib import array
+
+chacha20_test = TypedDict('blake2b_test', {
+    'data': str,
+    'key': str,
+    'nn': int,
+    'output': str
+    })
+
+blake2b_test_vectors = array([
+    {'data': '636261',
+    'key': '',
+    'nn': 64,
+    'output': 'ba80a53f981c4d0d6a2797b69f12f6e94c212f14685ac4b74b12bb6fdbffa2d17d87c5392aab792dc252d5de4533cc9518d38aa8dbf1925ab92386edd4009923'}
+])
