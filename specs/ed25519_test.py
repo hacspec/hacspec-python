69b2e5c1fd8a4e0d18093e595bed26a467eacd76
Natalia Kulatova <nkulatov@pl76-pro.paris.inria.fr>
2018-03-30 17:43:17 +0200

    Ed25519 + Vrf

---
+++ specs/ed25519_test.py
@@ -0,0 +1,32 @@
+from ed25519_2 import *
+import json
+#from test_vectors.ed25519_test_vectors import ed25519_test_vectors
+from sys import exit
+
+def main (x: int) -> None :
+    # RFC 7539 Test Vectors
+    pk0 = bytes.from_hex('d75a980182b10ab7d54bfed3c964073a0ee172f3daa62325af021a68f707511a')
+    sk0 = bytes.from_hex('9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60')
+    msg0 = bytes.from_hex('')
+    sig_expected = bytes.from_hex('e5564300c360ac729086e2cc806e828a84877f1eb8e5d974d873e065224901555fb8821590a33bacc61e39701cf9b46bd25bf5f0595bbe24655141438e7a100b')
+    sig_computed = sign(sk0,msg0)
+    verified = verify(pk0,msg0,sig_computed)
+    if (sig_expected == sig_computed):
+        print('Ed25518 Test 0 signature succeeded')
+    else:
+        print('Ed25518 Test 0 signature failed')
+        print('expected: '+str(sig_expected))
+        print('computed: '+str(sig_computed))
+    if (verified):
+        print('Ed25518 Test 0 verification succeeded')
+    else:
+        print('Ed25518 Test 0 verification failed')
+    verified = verify(pk0,msg0,sig_expected)
+    if (verified):
+        print('Ed25518 Test 0 verification_expected succeeded')
+    else:
+        print('Ed25518 Test 0 verification_expected failed')
+        
+main(0)
+
+
---
+++ specs/vrf.py
@@ -0,0 +1,104 @@
+from speclib import *
+from curve25519 import *
+from sha2 import *
+
+from ed25519_2 import *
+
+n = 16
+
+def OS2ECP(s:serialized_point_t) -> extended_point_t:
+	return point_decompress(s)
+
+def ECP2OS(p:extended_point_t) -> serialized_point_t :
+	return point_compress(p)
+
+def I2OSP(value: nat) -> bytes_t(32):
+	return bytes.from_nat_le(value)
+
+def OS2IP(s: serialized_scalar_t) -> felem_t:
+	return felem(bytes.to_nat_le(s))
+
+def hash (msg:vlbytes) -> nat:
+	return sha256(msg)
+
+curveOrder : felem_t = felem(7237005577332262213973186563042994240857116359379907606001950938285454250989)
+
+def ECVRF_hash_to_curve(ctr: nat, pub: serialized_point_t, input: bytes_t(uint32)) -> extended_point_t:
+	tmp = array.create(array.length(input)+64,uint8(0))
+	lenMessage = array.length(input)
+	tmp[:lenMessage] = input
+	tmp[lenMessage:lenMessage + 32] = pub
+	tmp[lenMessage + 32:] = I2OSP(ctr)
+
+	hashed = hash(tmp)
+	possiblePoint = OS2ECP(hashed)
+	if possiblePoint is None:
+		if ctr == curveOrderr:
+			return None
+		else:
+			return ECVRF_hash_to_curve(ctr + 1, pub, input)
+	else:
+		return possiblePoint		
+
+
+def ECVRF_decode_proof(pi: bytes_t(5*n)) -> (extended_point_t, bytes_t(n), bytes_t(2*n)):
+	gamma = pi[:2*n]
+	c = pi[2*n: 3*n]
+	s = pi[3*n: 5*n]
+	return (OS2ECP(gamma), c, s)
+
+def ECVRF_hash_points (g: extended_point_t, h: extended_point_t, pub: extended_point_t, gamma: extended_point_t, gk: extended_point_t, hk: extended_point_t) -> felem_t:
+	tmp = array.create(32*6, uint8(0))
+	tmp[0:32] = ECP2OS(g)
+	tmp[32:64] = ECP2OS(h)
+	tmp[64:96] = ECP2OS(pub)
+	tmp[96:128] = ECP2OS(gamma)
+	tmp[128:160] = ECP2OS(gk)
+	tmp[160:192] = ECP2OS(hk)
+	hashed = hash(tmp)
+	return felem(hashed)
+
+def ECVRF_prove (input: bytes_t(uint32), pub: serialized_point_t, priv: serialized_scalar_t, k:felem_t) -> (bytes_t(5*n)):
+	ap = point_decompress(pub)
+	if ap is None:
+		return False	
+	h = ECVRF_hash_to_curve(0, pub, input)
+	if h is None:
+		return None
+	gamma = point_mul(priv, h)
+	kPrime = I2OSP(k)
+	gk = point_mul(kPrime, g_ed25519)
+	hk = point_mul(kPrime, h)
+	c = ECVRF_hash_points(g_ed25519, h, ap, gamma, gk, hk)	
+	cPrime = felem_mul(c, (OS2IP (priv)))
+	s = felem_sub(k, cPrime)
+	tmp = array.create(5*n, uint8(0))
+	tmp[0:32] = ECP2OS(gamma)
+	tmp[32:48] = (I2OSP(c))[16:32]
+	tmp[48:5*n] = I2OSP(s)
+	return tmp
+
+def ECVRF_proof_to_hash (pi: bytes_t(5*n)) -> (bytes_t(2*n)):
+	(gamma, c, s) = ECVRF_decode_proof(pi)
+	h = hash(ECP2OS(gamma))
+	return I2OSP(h)
+
+def ECVRF_verify (pub: serialized_point_t, pi: bytes_t(5*n), input: bytes_t(uint32)) -> bool:
+	ap = point_decompress(pub)
+	if ap is None:
+		return False
+	(gamma, c, s) = ECVRF_decode_proof(pi)
+	if gamma is None:
+		return false
+	yc = point_mul(c, ap)
+	gs = point_mul(s, g_ed25519)
+	u = point_add(yc, gs)
+	h = ECVRF_hash_to_curve(0, pub, input)
+	if h is None:
+		false
+	gammac = point_mul(c, gamma)
+	hs = point_mul(s, h)
+	v = point_add(gammac, hs)
+	c_prime = ECVRF_hash_points(g_ed25519, h, ap, gamma, u, v)
+	halfC = c_prime % 2**128 
+	return halfC == OS2IP(c)		
