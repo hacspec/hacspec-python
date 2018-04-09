a58a55310dda9a167ab51ff835afd92ea2c41dd9
Denis Merigoux <denis.merigoux@gmail.com>
2018-04-09 11:39:47 +0200

    Added blake2s

--- specs/blake2s.py
+++ specs/blake2s.py
@@ -1,15 +1,15 @@
 from speclib import *
 
-bits_in_word = 64
-rounds_in_f = 12
-block_bytes = 128
-R1 = 32
-R2 = 24
-R3 = 16
-R4 = 63
+bits_in_word = 32
+rounds_in_f = 10
+block_bytes = 64
+R1 = 16
+R2 = 12
+R3 = 8
+R4 = 7
 
-working_vector_t = array_t(uint64_t, 16)
-hash_vector_t = array_t(uint64_t, 8)
+working_vector_t = array_t(uint32_t, 16)
+hash_vector_t = array_t(uint32_t, 8)
 index_t = range_t(0, 16)
 
 SIGMA: array_t(index_t, 16 * 12) = array([
@@ -23,38 +23,36 @@ SIGMA: array_t(index_t, 16 * 12) = array([
     13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10,
     6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5,
     10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0,
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-    14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3
 ])
 
 IV = array([
-    uint64(0x6A09E667F3BCC908), uint64(0xBB67AE8584CAA73B),
-    uint64(0x3C6EF372FE94F82B), uint64(0xA54FF53A5F1D36F1),
-    uint64(0x510E527FADE682D1), uint64(0x9B05688C2B3E6C1F),
-    uint64(0x1F83D9ABFB41BD6B), uint64(0x5BE0CD19137E2179)
+    uint32(0x6A09E667), uint32(0xBB67AE85), uint32(
+        0x3C6EF372), uint32(0xA54FF53A),
+    uint32(0x510E527F), uint32(0x9B05688C), uint32(
+        0x1F83D9AB), uint32(0x5BE0CD19)
 ])
 
 
-def G(v: working_vector_t, a: index_t, b: index_t, c: index_t, d: index_t, x: uint64_t, y: uint64_t) -> working_vector_t:
+def G(v: working_vector_t, a: index_t, b: index_t, c: index_t, d: index_t, x: uint32_t, y: uint32_t) -> working_vector_t:
     v[a] = v[a] + v[b] + x
-    v[d] = uint64_t.rotate_right(v[d] ^ v[a], R1)
+    v[d] = uint32_t.rotate_right(v[d] ^ v[a], R1)
     v[c] = v[c] + v[d]
-    v[b] = uint64_t.rotate_right(v[b] ^ v[c], R2)
+    v[b] = uint32_t.rotate_right(v[b] ^ v[c], R2)
     v[a] = v[a] + v[b] + y
-    v[d] = uint64_t.rotate_right(v[d] ^ v[a], R3)
+    v[d] = uint32_t.rotate_right(v[d] ^ v[a], R3)
     v[c] = v[c] + v[d]
-    v[b] = uint64_t.rotate_right(v[b] ^ v[c], R4)
+    v[b] = uint32_t.rotate_right(v[b] ^ v[c], R4)
     return v
 
 
-def F(h: hash_vector_t, m: working_vector_t, t: uint128_t, flag: bool) -> hash_vector_t:
-    v = array.create(16, uint64(0))
+def F(h: hash_vector_t, m: working_vector_t, t: uint64_t, flag: bool) -> hash_vector_t:
+    v = array.create(16, uint32(0))
     v[0:8] = h
     v[8:16] = IV
-    v[12] = v[12] ^ uint64(t)
-    v[13] = v[13] ^ uint64(t >> 64)
+    v[12] = v[12] ^ uint32(t)
+    v[13] = v[13] ^ uint32(t >> 32)
     if flag == True:
-        v[14] = v[14] ^ uint64(0xFFFFFFFFFFFFFFFF)
+        v[14] = v[14] ^ uint32(0xFFFFFFFF)
     for i in range(rounds_in_f):
         s = SIGMA[i * 16:(i + 1) * 16]
         v = G(v, 0, 4, 8, 12, m[s[0]], m[s[1]])
@@ -72,36 +70,37 @@ def F(h: hash_vector_t, m: working_vector_t, t: uint128_t, flag: bool) -> hash_v
 
 data_internal_t = refine(bytes_t, lambda x: vlbytes.length(
     x) < 2 ** 64 and (vlbytes.length(x) % block_bytes == 0))
-key_t = refine(vlbytes_t, lambda x: vlbytes.length(x) <= 64)
-key_size_t = refine(nat, lambda x: x <= 64)
+key_t = refine(vlbytes_t, lambda x: vlbytes.length(x) <= 32)
+key_size_t = refine(nat, lambda x: x <= 32)
 out_size_t = refine(nat, lambda x: x <= 32)
 
 
-def blake2b_internal(data: data_internal_t, input_bytes: uint128_t, kk: key_size_t, nn: out_size_t) \
+def blake2s_internal(data: data_internal_t, input_bytes: uint64_t, kk: key_size_t, nn: out_size_t) \
         -> contract(vlbytes_t,
                     lambda data, input_bytes, kk, nn: True,
                     lambda data, input_bytes, kk, nn, res: vlbytes.length(res) == nn):
     h = array.copy(IV)
-    h[0] = h[0] ^ uint64(0x01010000) ^ (uint64(kk) << 8) ^ uint64(nn)
+    h[0] = h[0] ^ uint32(0x01010000) ^ (uint32(kk) << 8) ^ uint32(nn)
     data_blocks = vlbytes.length(data) // block_bytes
     if data_blocks > 1:
         for i in range(data_blocks - 1):
-            h = F(h, vlbytes.to_uint64s_le(
-                data[block_bytes * i:block_bytes * (i + 1)]), uint128((i + 1) * block_bytes), False)
+            h = F(h, vlbytes.to_uint32s_le(
+                data[block_bytes * i:block_bytes * (i + 1)]), uint64((i + 1) * block_bytes), False)
     if kk == 0:
-        h = F(h, vlbytes.to_uint64s_le(
-            data[block_bytes * (data_blocks - 1):block_bytes * data_blocks]), uint128(input_bytes), True)
+        h = F(h, vlbytes.to_uint32s_le(
+            data[block_bytes * (data_blocks - 1):block_bytes * data_blocks]), uint64(input_bytes), True)
     else:
-        h = F(h, vlbytes.to_uint64s_le(
-            data[block_bytes * (data_blocks - 1):block_bytes * data_blocks]), uint128(input_bytes + block_bytes), True)
-    return vlbytes.from_uint64s_le(h)[:nn]
+        h = F(h, vlbytes.to_uint32s_le(
+            data[block_bytes * (data_blocks - 1):block_bytes * data_blocks]), uint64(input_bytes + block_bytes), True)
+    return vlbytes.from_uint32s_le(h)[:nn]
 
-max_size_t = 2**64 - 1
+
+max_size_t = 2**32 - 1
 data_t = refine(vlbytes_t, lambda x: vlbytes.lenght(x)
                 < max_size_t - 2 * block_bytes)
 
 
-def blake2b(data: data_t, key: key_t, nn: out_size_t) \
+def blake2s(data: data_t, key: key_t, nn: out_size_t) \
         -> contract(vlbytes_t,
                     lambda data, key, nn: True, lambda data, key, nn, res: vlbytes.length(res) == nn):
     ll = vlbytes.length(data)
@@ -114,5 +113,5 @@ def blake2b(data: data_t, key: key_t, nn: out_size_t) \
     else:
         padded_data = array.create(padded_data_length + block_bytes, uint8(0))
         padded_data[0:kk] = key
-        padded_data[block_bytes:block_bytes+ll] = key
-    return blake2b_internal(padded_data, ll, kk, nn)
+        padded_data[block_bytes:block_bytes + ll] = key
+    return blake2s_internal(padded_data, ll, kk, nn)
--- specs/blake2s_test.py
+++ specs/blake2s_test.py
@@ -1,19 +1,19 @@
-from blake2b import *
+from blake2s import *
 
-from test_vectors.blake2b_test_vectors import *
+from test_vectors.blake2s_test_vectors import *
 from sys import exit
 
 def main(x:int) -> None:
-    for i, vec in enumerate(blake2b_test_vectors):
+    for i, vec in enumerate(blake2s_test_vectors):
         data = bytes.from_hex(vec['data'])
         key = bytes.from_hex(vec['key'])
         nn = vec['nn']
         expected = bytes.from_hex(vec['output'])
-        computed = blake2b(data,key,nn)
+        computed = blake2s(data,key,nn)
         if computed == expected:
-            print("Blake2b Test {} passed.".format(i+1))
+            print("Blake2s Test {} passed.".format(i+1))
         else:
-            print("Blake2b Test {} failed.".format(i+1))
+            print("Blake2s Test {} failed.".format(i+1))
             print("expected ciphertext:",bytes.to_hex(expected))
             print("computed ciphertext:",bytes.to_hex(computed))
             exit(1)
