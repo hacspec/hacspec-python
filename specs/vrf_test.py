a97cb6e7227c1849daeab8ab76b240de8e4756ed
Franziskus Kiefer <franziskuskiefer@gmail.com>
2018-04-16 10:57:23 +0200

    long running travis jobs

--- specs/vrf_test.py
+++ specs/vrf_test.py
@@ -2,7 +2,6 @@ from vrf import *
 from sys import exit
 
 def main (x: int) -> None :
-
     sk0 = bytes.from_hex('9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60')
     pk0 = ECP2OS(point_mul(sk0, g_ed25519))
     msg0 = bytes.from_hex('')
