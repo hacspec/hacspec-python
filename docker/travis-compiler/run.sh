207903728ed2a0eaf66b0163c0e517a47b326631
Franziskus Kiefer <franziskuskiefer@gmail.com>
2018-08-12 17:58:42 +0200

    add docker service :/

--- docker/travis-compiler/run.sh
+++ docker/travis-compiler/run.sh
@@ -2,10 +2,6 @@
 
 set -v -x
 
-# Copy hacspec into the docker image.
-# This is necessary because of weird errors travis throws otherwise.
-cp -r /home/worker/_hacspec /home/worker/hacspec
-
 # Run compilers and type check.
 cd /home/worker/hacspec/spec-checker-ocaml
 make
