9dead6756ac6cd1262b6ceacd01568b37df81914
Pierre-Yves Strub <pierre-yves@strub.nu>
2018-08-21 08:51:21 +0200

    hacs-ocaml: make it a library

--- spec-checker-ocaml/libs/hacs/checker.ml
+++ spec-checker-ocaml/libs/hacs/checker.ml
@@ -1,13 +1,13 @@
 (* -------------------------------------------------------------------- *)
-open Hacs.Core
+open Core
 
-module P = Hacs.Reader
-module T = Hacs.Typing
+module P = Reader
+module T = Typing
 
 (* -------------------------------------------------------------------- *)
 let main () =
   if Array.length Sys.argv - 1 <> 1 then begin
-    Format.eprintf "Usage: %s [filename]\n%!" Sys.argv.(0);
+    Format.eprintf "Usage: %s [filename]" Sys.argv.(0);
     exit 2
   end;
 
@@ -20,7 +20,7 @@ let main () =
         (P.parse_intf %> T.tt_interface T.Env.empty0)
         (P.from_file (Resource.getlib "speclib.pyi")) in
 
-    let (_ : T.Env.env * T.Env.env Hacs.Ast.program) =
+    let (_ : T.Env.env * T.Env.env Ast.program) =
       with_dispose ~dispose:P.finalize
         (P.parse_spec %> T.tt_program env)
         (P.from_file filename)
@@ -31,7 +31,7 @@ let main () =
       modname
   with
   | e ->
-      Format.eprintf "%s%!" (Hacs.Pexception.tostring e);
+      Format.eprintf "%s%!" (Pexception.tostring e);
       exit 1
 
 (* -------------------------------------------------------------------- *)
